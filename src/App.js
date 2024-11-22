f14b0b6216b70d2880b9431ea02c04519eb6706d
MithunGowda <mithungowdahc12a@gmail.com>
2024-11-22 15:45:52 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import "./App.css";
+
+import Mainhodblock from "./hod/Mainhodblock";
+// import Mainteacherblock from "./teacherblock/Mainteacherblock";
+// import Mainstudentblock from "./Student/Mainstudentblock";
+import FacultyLogin from "./Login/FacultyLogin";
+import Signup from "./Login/Signup";
+import Newstate from "./context/Newstate";
+import FacultyState from "./context/FacultyState";
+import Landing from "./components/Landing";
+import Mainlanding from "./Student/Mainlanding";
+import MainteacherLanding from "./teacherblock/MainteacherLanding";
+import { ModalState } from "./context/Modalcontext";
+function App() {
+
+
+  return (
+    <>
+      <ModalState>
+        <FacultyState>
+          <Newstate>
+
+
+            <Router>
+              <Routes>
+                <Route exact path="/" element={<Landing />} />
+                <Route exact path="/mnm/" element={<Signup />} />
+                <Route path="/mnm/faculty" element={<FacultyLogin />} />
+                {/* <Route path="/mnm/student" element={<Mainstudentblock />} /> */}
+                <Route path="/mnm/student" element={<Mainlanding />} />
+                <Route path="/mnm/HOD" element={<Mainhodblock />} />
+                <Route path="/mnm/teacher" element={<MainteacherLanding />} />
+              </Routes>
+            </Router>
+          </Newstate>
+        </FacultyState>
+      </ModalState>
+
+    </>
+  );
+
+
+
+}
+
+export default App;
+
+
+
+
+
